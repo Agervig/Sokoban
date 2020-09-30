7aba5622f0cc36fefa942ba55ad699f33012ec6d
Thomas Jensen <thje016@student.sdu.dk>
2020-09-30 10:57:55 +0200

    Initial commit

---
+++ basic_control.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+#Venstre motor går til OUTPUT_A, højre OUTPUT_B,
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, MoveTank, MoveSteering
+from ev3dev2.sensor import INPUT_1, INPUT_2,INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, LightSensor, GyroSensor
+from time import sleep
+from ev3dev2.sound import Sound
+
+class CarControl:
+    def __init__(self):
+        self.tank_pair = MoveTank(OUTPUT_A, OUTPUT_B)
+        self.steer_pair = MoveSteering(OUTPUT_A, OUTPUT_B)
+        self.line_sensor = ColorSensor(INPUT_2)
+        self.line_counter = ColorSensor(INPUT_3)
+        self.sound = Sound()
+
+
+    def drive(self, l_speed, r_speed):
+        self.tank_pair.on(left_speed=l_speed, right_speed=r_speed)
+
+    def brake(self):
+        self.tank_pair.off(brake = True)
+
+    def turn_left(self):
+        self.steer_pair.on_for_degrees(steering = -100, speed = 90, degrees=180, brake=True)
+
+    def turn_right(self):
+        self.steer_pair.on_for_degrees(steering = 100, speed = 90, degrees=170, brake=True)
+
+    def turn_arround(self):
+        self.steer_pair.on_for_degrees(steering = -100, speed = 90, degrees = 370, brake = True)
+
+    def read_line_sensor(self):
+        return (self.line_sensor.reflected_light_intensity)
+    
+    def read_line_counter(self):
+        return (self.line_counter.reflected_light_intensity)
+
+    def play_sound(self):
+        self.sound.beep()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import basic_control
+
+
+class CarBehavior:
+    def __init__(self, ti):
+        self.target_intesity = ti    
+        self.factor = 1
+        self.cc = basic_control.CarControl()
+        
+         
+
+    def lineFollow(self, speed):
+         #Scaling down the intensity, expected range: 2 - 22 - 34   (range of 20 for black, range of 12 for white)
+        intensity = self.cc.read_line_sensor()
+        if intensity > self.target_intesity:
+            speed_correction = round((self.target_intesity-intensity)*self.factor)
+            self.cc.drive(speed + speed_correction, speed - speed_correction)
+        else:
+            speed_correction = round(((self.target_intesity - intensity)/2)*self.factor)
+            self.cc.drive(speed + speed_correction, speed - speed_correction)
+
+    
+    def cros_line_state(self):
+        white = False
+        white_counter = 0
+        while white == False:
+            self.cc.drive(70, 70)
+            if self.cc.read_line_counter() > 30:
+                white_counter += 1
+            if white_counter > 5:
+                white = True
+
+    #Kan være der skal tilføjes så vi kigger på eksempelvis de sidste 5-10 værdier fra line counter sensor
+    #før vi registrerer det som en linje (Hvis værdien nu ved en fejl jumper ned til sort)..
+    def straigt(self, lines):
+        while lines > 0:
+            self.lineFollow(70)
+            if self.cc.read_line_counter() < 5:
+                self.cros_line_state()    
+                lines = lines - 1
+        self.cc.brake()
+
+
+
+
+
+                    
+        
+        
+        
+
+test = CarBehavior(18)
+test.cc.play_sound()
+test.straigt(2)
+test.cc.turn_right()
+test.straigt(1)
+test.cc.turn_right()
+test.straigt(1)
+
+
+
